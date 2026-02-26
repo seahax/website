5806cad745faa46de318ac2b42a7dce46b9302fb
Chris Ackerman <chris@topher.land>
2026-02-25 22:23:01 -0800

    rename terraform/doks/helm to .../cert-manager

--- terraform/doks/cert-manager/_config.tf
+++ terraform/doks/cert-manager/_config.tf
@@ -2,7 +2,7 @@ terraform {
   backend "kubernetes" {
     config_path    = "~/.kube/config"
     config_context = "do-sfo3-seahax"
-    secret_suffix  = "helm-state"
+    secret_suffix  = "cert-manager-state"
   }
 }
 
